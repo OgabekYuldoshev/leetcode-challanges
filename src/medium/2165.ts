fc4d7edefbb4e472034b0a13dbce209a56fb93c3
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-09-10 16:50:37 +0500

    feat: ⚡️ SOLVED: 2165. Smallest Value of the Rearranged Number

---
+++ src/medium/2165.ts
@@ -0,0 +1,27 @@
+// 2165. Smallest Value of the Rearranged Number
+
+// Submittion: https://leetcode.com/problems/smallest-value-of-the-rearranged-number/submissions/1385318688/
+
+function smallestNumber(num: number): number {
+  let number = Math.abs(num);
+
+  if (num < 0) {
+    const digets = [...number.toString()].map(Number).sort((a, b) => b - a);
+    return -digets.join("");
+  } else {
+    const digets = [...number.toString()].map(Number).sort((a, b) => a - b);
+
+    if (digets[0] === 0) {
+      for (let i = 0; i < digets.length; i++) {
+        if (digets[i] < digets[i + 1]) {
+          [digets[0], digets[i + 1]] = [digets[i + 1], digets[i]];
+          break;
+        }
+      }
+    }
+
+    return +digets.join("");
+  }
+}
+
+console.log(smallestNumber(63));
