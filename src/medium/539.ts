af3a0d1c54c27bd2cc6891fbaca195eb5ea5b4a5
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-09-17 10:03:50 +0500

    feat: ⚡️ SOLVED: 539. Minimum Time Difference

---
+++ src/medium/539.ts
@@ -0,0 +1,35 @@
+// 539. Minimum Time Difference
+
+// Solved: https://leetcode.com/problems/minimum-time-difference/submissions/1392852791/?envType=daily-question&envId=2024-09-16
+
+function findMinDifference(timePoints: string[]): number {
+    let minutes = new Array(timePoints.length)
+
+    for (let i = 0; i < timePoints.length; i++) {
+        const time = timePoints[i]
+
+        const h = time.substring(0, 2)
+
+        const m = time.substring(time.length - 2, time.length)
+
+        minutes[i] = Number(h) * 60 + Number(m)
+    }
+
+    minutes = minutes.sort((a, b) => a - b)
+
+    let ans = Number.MAX_SAFE_INTEGER
+
+    for (let i = 0; i < minutes.length - 1; i++) {
+        ans = Math.min(ans, minutes[i + 1] - minutes[i])
+    }
+
+    console.log(ans)
+
+    let diff = 24 * 60 - minutes[minutes.length - 1] + minutes[0]
+
+    return Math.min(ans, diff)
+};
+
+let timePoints = ["12:12", "12:13"]
+
+console.log(findMinDifference(timePoints))
