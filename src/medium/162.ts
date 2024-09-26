a0c390e0cede8311b2ba2961ff962672edde4d3c
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-09-26 16:56:37 +0500

    feat(SOLVED): 🐛 162. Find Peak Element

---
+++ src/medium/162.ts
@@ -0,0 +1,37 @@
+// 162. Find Peak Element
+
+// Solution: https://leetcode.com/problems/find-peak-element/submissions/1402861259/?source=submission-noac
+
+function findPeakElement(nums: number[]): number {
+    if (nums.length === 1) return 0;
+
+    let ans = 0;
+    let left = 0;
+    let right = nums.length - 1;
+
+    let mid = 0;
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        const current = nums[mid];
+        const rightSide = nums[mid + 1];
+
+        if (current > rightSide) {
+            ans = mid;
+            right = mid - 1;
+        }
+
+        if (current < rightSide) {
+            left = mid + 1;
+        }
+
+        if (rightSide === undefined) {
+            ans = mid;
+            break;
+        }
+    }
+
+    return ans
+}
+
+console.log(findPeakElement([1, 2, 1]));
