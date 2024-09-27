27b3920c7188b8890a34585e5f52177ebe154130
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-09-27 09:03:07 +0500

    feat(solved): ⚡️ 2951. Find the Peaks

---
+++ src/easy/2951.ts
@@ -0,0 +1,19 @@
+// 2951. Find the Peaks
+// Solution: https://leetcode.com/problems/find-the-peaks/submissions/1403562998/
+function findPeaks(mountain: number[]): number[] {
+    const ans: number[] = [];
+
+    for (let i = 0; i < mountain.length - 2; i++) {
+        const left = mountain[i];
+        const current = mountain[i + 1];
+        const right = mountain[i + 2];
+
+        if (current > left && current > right) {
+            ans.push(i + 1)
+        }
+    }
+
+    return ans;
+}
+
+console.log(findPeaks([2, 4, 4]));
