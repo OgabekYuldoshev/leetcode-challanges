1856f77a7cab71ec843b0998b5083315de89c92f
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-10-07 18:56:56 +0500

    feat(solved): ⚡️ 989. Add to Array-Form of Integer

---
+++ src/easy/989.ts
@@ -0,0 +1,22 @@
+// 989. Add to Array-Form of Integer
+// Solution: https://leetcode.com/problems/add-to-array-form-of-integer/submissions/1414799323/
+function addToArrayForm(num: number[], k: number): number[] {
+    let i = num.length - 1;
+
+    while (i >= 0) {
+        const sum = num[i] + k;
+        k = Math.floor(sum / 10)
+        num[i] = sum % 10
+        i -= 1;
+    }
+
+    while (k > 0) {
+        const sum = k % 10
+        k = Math.floor(k / 10)
+        num.unshift(sum)
+    }
+
+    return num;
+}
+
+console.log(addToArrayForm([0], 10000));
