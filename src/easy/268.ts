65ada41ffb40612fc9bb9cda4274519631b5ad32
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-10-16 09:40:27 +0500

    feat(solved): ⚡️ 268. Missing Number

---
+++ src/easy/268.ts
@@ -0,0 +1,9 @@
+// 268. Missing Number
+// Solved: https://leetcode.com/problems/missing-number/submissions/1423895290/
+function missingNumber(nums: number[]): number {
+    let sum = nums.reduce((acc, curr) => acc + curr, 0);
+    let expectedSum = nums.length * (nums.length + 1) / 2;
+    return expectedSum - sum;
+};
+
+console.log(missingNumber([3, 0, 1]))
