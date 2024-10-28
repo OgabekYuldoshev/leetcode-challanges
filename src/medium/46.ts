f721ec3a9b23bd64d516b06fef9cc5eb8da30d08
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-10-28 15:35:39 +0500

    feat: 🔥 46. Permutations

---
+++ src/medium/46.ts
@@ -0,0 +1,24 @@
+// 46. Permutations
+
+function permute(nums: number[]): number[][] {
+    const map: number[][] = []
+
+    function recursion(arr: number[], idx: number) {
+        if (idx === arr.length) {
+            map.push([...arr]);
+            return;
+        }
+
+        for (let i = idx; i < arr.length; i++) {
+            [arr[idx], arr[i]] = [arr[i], arr[idx]]
+            recursion(arr, idx + 1);
+            [arr[idx], arr[i]] = [arr[i], arr[idx]]
+        }
+    }
+
+    recursion(nums, 0)
+
+    return map
+};
+
+console.log(permute([1, 2, 3]))
