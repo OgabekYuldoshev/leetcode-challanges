684046cffecb4dfa73233628b3fdda554141c3ef
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-11-05 16:20:53 +0500

    feat(solved): 🚑️ 2914. Minimum Number of Changes to Make Binary String Beautiful

---
+++ src/medium/2914.ts
@@ -0,0 +1,15 @@
+
+// 2914. Minimum Number of Changes to Make Binary String Beautiful
+// Solved: https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful/?envType=daily-question&envId=2024-11-05
+
+function minChanges(s: string): number {
+    let count = 0
+
+    for (let i = 0; i < s.length; i += 2) {
+        if (s[i] !== s[i + 1]) count++
+    }
+
+    return count
+};
+
+console.log(minChanges("1001"))
